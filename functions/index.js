724e60e3c5bf2f5f5fbcbaf0bdba239184385733
martino3232 <ezequielmartino3279@gmail.com>
2025-07-19 20:35:43 -0300

    fix: server

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://francislaboratoriofotografico.onrender.com"
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
